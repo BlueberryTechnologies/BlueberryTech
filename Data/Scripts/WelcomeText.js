05eea723f54bd19a6f8ec788e6b20f67ddbbad5a
Joystick <bubbalou112@gmail.com>
2022-10-10 01:00:44 -0400

    Version a3.1
    - Added implementation of Typed.js and JQuery
    - Removed Dark Mode (For now)
    - Various margin, border, width changes.
    - Added image link back to homepage.

---
+++ Data/Scripts/WelcomeText.js
@@ -0,0 +1,14 @@
+// Using Typed
+var typed = new Typed("#WelcomeHeader", {
+	strings: [' ','Blueberry Tech.'],
+	typeSpeed: 275,
+	showCursor: false,
+	onComplete: function(self) {
+		document.getElementById("WelcomeHeader2").style.color = "black";
+		var simplyExperienced = new Typed("#WelcomeHeader2", {
+			strings: [' ', 'Simply Experienced.'],
+			typeSpeed: 250,
+			showCursor: false
+		})
+	}
+});

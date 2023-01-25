f39b5074dd41e1833bb1cbe4a5b60a580126fd75
Riley Richard <bubbalou112@gmail.com>
2023-01-24 22:59:19 -0500

    mobile support pt 1

---
+++ tech/Data/Scripts/Sidebar.js
@@ -0,0 +1,7 @@
+/* Control for the mobile sidebar */
+function openSidebar() {
+  document.getElementsByClassName("").style.display = block;
+}
+function openSidebar() {
+  document.getElementsByClassName("").style.display = none;
+}

b89e072c221f3184b89932cb4b3df9171bddcd53
Riley <bubbalou112@gmail.com>
2024-03-28 22:14:10 -0400

    new page

---
+++ berry-site/src/Landing.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import blueberryLogo from "../data/Images/logotransparent.png";
+import githubLogo from "./assets/github-mark.svg";
+import "./Landing.css";
+
+function Landing() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://blueberry.dev" target="_blank">
+          <img src={blueberryLogo} className="logo" alt="Blueberry logo" />
+        </a>
+      </div>
+      <h1>Blueberry Technologies</h1>
+      <div className="card">
+        <button>
+          <a href="https://github.com/BlueberryTechnologies" target="_blank">
+            <img src={githubLogo} className="githubLogo"></img>
+          </a>
+        </button>
+      </div>
+      <p className="read-the-docs">© 2024 Blueberry Technologies</p>
+    </>
+  );
+}
+
+export default Landing;
